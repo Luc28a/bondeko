e0e37dcdaae6ff1fce02e04e37b94bc21a42ee38
Luc28a <Luc32082@gmail.com>
2026-08-02 02:55:23 +0200

    Update send_notification.js

--- .github/scripts/send_notification.js
+++ .github/scripts/send_notification.js
@@ -1,1 +1,31 @@
+const admin = require("firebase-admin");
 
+const serviceAccount = require("../../firebase-key.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
+const message = {
+  notification: {
+    title: "Mise à jour Bondeko",
+    body: "Nouvelle donnée disponible",
+  },
+
+  data: {
+    action: "sync",
+  },
+
+  topic: "bondeko_updates",
+};
+
+admin
+  .messaging()
+  .send(message)
+  .then((response) => {
+    console.log("Notification envoyée :", response);
+  })
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
